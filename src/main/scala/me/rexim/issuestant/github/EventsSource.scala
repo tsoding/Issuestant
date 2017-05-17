e05384e41a64b1ad1c1e6d23d1f835364208177c
rexim <reximkut@gmail.com>
2017-05-18 00:18:19 +0700

    Implement Polling Iteration (#47)

--- src/main/scala/me/rexim/issuestant/github/EventsSource.scala
+++ src/main/scala/me/rexim/issuestant/github/EventsSource.scala
@@ -10,11 +10,13 @@ import scalaz.concurrent._
 import me.rexim.issuestant.github.model.Event
 
 import org.http4s._
+import org.http4s.util._
 import org.http4s.client._
 import org.http4s.Http4s._
 import org.http4s.Status._
 import org.http4s.Method._
 import org.http4s.EntityDecoder
+import scalaz._
 
 /** The source of GitHub events
   *
@@ -27,17 +29,31 @@ import org.http4s.EntityDecoder
   */
 // $COVERAGE-OFF$
 class EventsSource (client: Client, owner: String, repo: String) {
+
+  // private lazy val githubApiUrl = s"http://asoftmurmur.com/"
+  private lazy val \/-(githubApiUri) = Uri.fromString(s"https://api.github.com/repos/$owner/$repo/issues/events")
+
   // TODO(4369af78-08f6-45d7-8399-c5ef1f97808a): Get rid of wart suppress
   @SuppressWarnings(Array("org.wartremover.warts.Any"))
-  def events: Process[Task, Event] =
-    Process
-      .iterateEval(Response())(pollingIteration)
-      .flatMap((response) => extractEvents(response))
+  def events: Process[Task, Event] = responses.flatMap(extractEvents)
 
-  // TODO(e4581c14-8db7-4e1b-90f0-0b8faec52a33): Implement EventsSource.pollingIteration
-  //
-  // Extract ETag from the previousResponse and use it for polling the next events
-  private def pollingIteration(previousResponse: Response): Task[Response] = ???
+  private def responses: Process[Task, Response] =
+    Process.iterateEval(Response())(pollingIteration)
+
+  private def getETag(response: Response): Option[String] =
+    response.headers.get(CaseInsensitiveString("ETag")).map(_.value)
+
+  private def nextRequest(previousResponse: Response): Request =
+    Request (
+      uri = githubApiUri,
+      headers =
+        getETag(previousResponse)
+          .map(e => Headers(Header("If-None-Match", e)))
+          .getOrElse(Headers.empty)
+    )
+
+  def pollingIteration(previousResponse: Response): Task[Response] =
+    client.fetch[Response](nextRequest(previousResponse))((x) => Task(x))
 
   // TODO(6900c3e4-7c15-42cc-9389-de3947625ce9): Implement EventsSource.extractEvents
   private def extractEvents(response: Response): Process[Task, Event] = ???
