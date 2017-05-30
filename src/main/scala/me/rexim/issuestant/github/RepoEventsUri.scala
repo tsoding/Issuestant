17dc61533300cbc402f7c1513347142eb0642a1e
rexim <reximkut@gmail.com>
2017-05-30 15:30:31 +0700

    URI encode repo events URI components (#47)

--- src/main/scala/me/rexim/issuestant/github/RepoEventsUri.scala
+++ src/main/scala/me/rexim/issuestant/github/RepoEventsUri.scala
@@ -5,7 +5,9 @@ import org.http4s._
 
 class RepoEventsUri(owner: String, repo: String) {
   private lazy val \/-(githubApiUri) =
-    Uri.fromString(s"https://api.github.com/repos/$owner/$repo/issues/events")
+    Uri.fromString("https://api.github.com/").map { baseUri =>
+      baseUri / "repos" / owner / repo / "issues"/ "events"
+    }
 
   def asUri: Uri = githubApiUri
 }
