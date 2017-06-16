b3e5d08edcd5f20cf522e12a3c2362304329697b
rexim <reximkut@gmail.com>
2017-06-16 13:55:48 +0700

    Assert /rexim is actually requested (#51)

--- src/test/scala/me/rexim/github/EtagPollingSpec.scala
+++ src/test/scala/me/rexim/github/EtagPollingSpec.scala
@@ -10,15 +10,19 @@ class EtagPollingSpec extends FlatSpec with Matchers {
   behavior of "EtagPolling"
 
   it should "reuse etag header for the service response" in {
+    val requestCount = 5
+    val requestUri = Uri(path = "/rexim")
+
     val etagLogging = new EtagLogging
     val etagPolling = new EtagPolling(
       etagLogging.client,
-      Uri(path = "/rexim")
+      requestUri
     )
 
-    val responses = etagPolling.responses.take(5).runLog.run.toList
+    val responses = etagPolling.responses.take(requestCount).runLog.run.toList
     val requests = etagLogging.log
 
+    requests.map(_.uri) should be ((1 to requestCount).map(_ => requestUri))
     (Nil :: responses.map(_.headers.map(_.value)).init) should be (requests.map(_.headers.map(_.value)))
   }
 }
