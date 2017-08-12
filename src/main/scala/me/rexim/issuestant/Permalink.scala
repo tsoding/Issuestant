a20ca3c5eb894b1461ed52d2fd9e6f9644d17e43
rexim <reximkut@gmail.com>
2017-08-13 00:42:26 +0700

    Make Permalink service specific rather than generic (#66)

--- src/main/scala/me/rexim/issuestant/Permalink.scala
+++ src/main/scala/me/rexim/issuestant/Permalink.scala
@@ -33,8 +33,8 @@ import org.http4s._
   * @param eventSource the source of GitHub events
   */
 // $COVERAGE-OFF$
-class Permalink[E](eventSource: ActivityEventsSource) {
-  def asTask(implicit decoder: Decoder[E]): Task[Unit] = Task {
+class Permalink(eventSource: ActivityEventsSource) {
+  def asTask: Task[Unit] = Task {
     val _ = eventSource.events.runLog.run
   }
 }
